4c08d840ab4c0d1ff3dc7ba8624a4b699c6c140e
Henrique <45628939+henrique7b@users.noreply.github.com>
2019-02-15 16:46:30 -0200

    Update app.js
    Explicações melhoradas e código mais limpo

--- encode/app.js
+++ encode/app.js
@@ -1,52 +1,49 @@
-// Codificando:
-// 1. Criar uma matriz com a string de tamanho n x n, (n = string.length)
-// 2. A linha consecultiva da matriz deve ser composta pela rotação de um elemento da linha anterior (abcd > dabc > cdab > bcda)
-// 3. Ordenar as linhas da matriz alfabeticamente (abcd, bcda, cdab, dabc)
-// 4. A saída deverá ser, por exemplo: matriz["abcd", 0] = dabc
-
-function criarArray(palavra) {
+function rotacionarPalavra(palavra) {
     var str = "";
     var strAux = palavra;
     var data = "";
 
-    // Rotacionando os valores
+    // Rotacionando os valores e os adicionando ao array
     for (var i = 0; i < palavra.length; i++) {
         str = strAux;
         txtAux[i] = str.charAt(str.length - 1) + str.substr(0, str.length - 1);
         strAux = txtAux[i];
     }
-    // Organizando os valores
+
+    // Organizando os valores do array
     txtAux.sort();
 
-    // Criando o valor que será atribuído a saída do arquivo, exemplo: matriz[abracadabra, 2] = "rdarcaaaabb";
-    for (var i = 0; i < palavra.length; i++) {
-        data += txtAux[i].charAt(palavra.length - 1);
-    }
-    saidaArquivo(data, palavra);
+    // Criando o valor que será atribuído a saída do arquivo. "txtAux[i].charAt(palavra.length - 1)" está 'simulando' a última coluna de cada linha
+    for (var i = 0; i < palavra.length; i++) { data += txtAux[i].charAt(palavra.length - 1); }
+    rotacionarPalavraPalavra(palavra, data);
 }
 
-function saidaArquivo(data, palavra) {
+function rotacionarPalavraPalavra(palavra, data) {
+
+    // Ciando uma RE para identificar o valor obtido na entrada "./encode-n" e poder salvar o valor codificado
     var re = new RegExp(palavra, "g");
 
     for (var i = 0; i < palavra.length; i++) {
+
+        // Criando a codificação
         m[txtAux[i], i];
         if (re.test(txtAux[i])) {
             m[txtAux[i], i] = data;
-            console.log("| Linha: " + txtAux[i] + " | Coluna: " + i + " | Conteúdo: " + m[txtAux[i], i] + " |");
+            console.log("Entrada: " + txtAux[i] + " | Saída: [\"" + m[txtAux[i], i] + "\", " + i + "]");
             fs.writeFileSync(nomeArquivo + ".out", "[\"" + m[txtAux[i], i] + "\", " + i + "]", "UTF-8")
         }
     }
 }
 
 const fs = require('fs');
-const nomeArquivo = "./encode-1";   // Faça a alteração de n 'encode-n' manualmente (1 até 4), de acordo com os arquivos existentes no diretório
+const nomeArquivo = "./encode-4";   // Altere o './encode-n' para n = '1', '2', '3' ou '4'. './encode-1' foi utilizado apenas como testes
 var txt = "";
 var m = [];
 var txtAux = [];
 
 try {
     txt = fs.readFileSync(nomeArquivo + ".in", 'UTF-8');
-    criarArray(txt);
+    rotacionarPalavra(txt);
 
 } catch (err) {
     console.log(err.name);
