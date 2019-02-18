5454e6678a60302c0803bea7059fdc2a9ec6d38a
Henrique <45628939+henrique7b@users.noreply.github.com>
2019-02-17 21:56:39 -0300

    Alguns tratamentos implementados
    O código ainda está incompleto

--- problema-extra/app.js
+++ problema-extra/app.js
@@ -1,85 +1,88 @@
-// Identificar o tipo de fórmula
-function separarFormula(par) {
+// Identificando o tipo de fórmula
+function separarFormula() {
 
-    if (!par) return;
+    if (!formula) return;
 
-    if (/\{.*\}/.test(par)) {
-        console.log("if 1.1");
-        formula = /\{.*\}/.exec(par).toString();
-        multi = (/\}\d+/.test(par)) ? /\}\d+/.exec(par).toString() : 1;
+    if (/\(.*\)/.test(formula)) {
+        console.log("if com ( )");
+        elemento = /\(.*\)\d+?/.exec(formula).toString();                                       // Definindo o elemento
+        multi = (/\)\d+/.test(formula)) ? /\)\d+/.exec(formula).toString().substring(1) : "1";  // Multiplicador
 
+        formula = formula.replace(elemento, "");                                                // Removendo o elemento da fórmula
+        elemento = elemento.substring(1, elemento.indexOf(/\)/.exec(elemento).toString()));     // Retirando caracteres especiais do elemento
+        multi = Number(multi);                                                                  // Tratando o dado para ser alocado como number
+
+        tratarFormula(elemento, multi);
+    }
+
+    if (/\[.*\]/.test(formula)) {                               // Método não testado
+        console.log("if com [ ]");
+        formula = /\[.*\]/.exec(formula).toString();
+        multi = (/\]\d+/.test(formula)) ? /\]\d+/.exec(formula).toString().substring(1) : "1";
+
+        elemento = formula.replace(formula, "");
         formula = formula.substring(1, formula.length - 1);
-        multi = Number(multi.substring(1));
+        multi = Number(multi);
+
+        tratarFormula(formula, multi);
+    }
+
+    if (/\{.*\}/.test(formula)) {
+        console.log("if com { }");
+        formula = /\{.*\}/.exec(formula).toString();
+        multi = (/\}\d+/.test(formula)) ? /\}\d+/.exec(formula).toString().substring(1) : "1";
+
+        elemento = formula.replace(formula, "");
+        formula = formula.substring(1, formula.length - 1);
+        multi = Number(multi);
+
         console.log("formula: " + formula + typeof formula);
         console.log("multiplicador: " + multi + typeof multi);
         tratarFormula(formula, multi);
     }
 
-    if (/\[.*\]/.test(par)) {
-        console.log("if 1.2");
-        formula = /\[.*\]/.exec(par).toString();
-        multi = (/\]\d+/.test(par)) ? /\]\d+/.exec(par).toString() : 1;
+    if (/^[\w]+/.test(formula)) {
+        console.log("if sem c. especial");
+        elemento = /.*/.exec(formula).toString();
 
-        formula = formula.substring(1, formula.length - 1);
-        multi = Number(multi.substring(1));
-        console.log("formula: " + formula + typeof formula);
-        console.log("multiplicador: " + multi + typeof multi);
-        tratarFormula(formula, multi);
-    }
-
-    if (/\(.*\)/.test(par)) {
-        console.log("if 1.3");
-        formula = /\(.*\)/.exec(par).toString();
-        multi = (/\)\d+/.test(par)) ? /\)\d+/.exec(par).toString() : 1;
-
-        formula = formula.substring(1, formula.length - 1);
-        multi = Number(multi.substring(1));
-        console.log("formula: " + formula + typeof formula);
-        console.log("multiplicador:" + multi + typeof multi);
-        tratarFormula(formula, multi);
-    }
-
-    if (/^[\w]+/.test(par)) {
-        console.log("if 1.3");
-        formula = /^[\w]+/.exec(par).toString();
-
-        console.log("formula: " + formula + typeof formula);
-        tratarFormula(formula, 1);
+        formula = formula.replace(elemento, "");
+        tratarFormula(elemento, "1");
     }
 
 
+    //TODO: implementar/alterar tratamentos para fórmulas como Mg(OH)2
 
-    //TODO: criar maneira para reencaminhar a palavra após alguma parte ter sido removida dela
 
+    separarFormula(elemento);
 }
 
 // Organização e contagem dos valores
 function tratarFormula(par, multiplicador) {
     var str = par;
-    var multi = multiplicador;
+    var num = multiplicador;
 
     do {
         if (/[\w]+\d/.test(str)) {
             console.log("if 2");
-            if (/^[A-Z][A-Z]/.test(str)) {
+
+            // Verificar como irá ficar quando ocorrer mais de 2 números em caps
+
+            if (/^[A-Z][A-Z]/.test(str)) {  // if não testado
                 console.log("if 2.1");
-                x[count++] = [/^[A-Z]/.exec(str), 1];                       // Verificar como irá ficar quando ocorrer mais de 2 números em caps
-                x[count++] = [/\D\d/.exec(str), /\d+/.exec(str) * multi];   // Usar \D ?
+                x[count++] = [/^[A-Z]/.exec(str), 1];
+                x[count++] = [/\D\d/.exec(str), /\d+/.exec(str) * num];
                 str = str.slice(2);                                         // Corrigir o slice
-                console.log(str);
             }
-            if (/^[A-Z][a-z]/.test(str)) {
+            if (/^[A-Z][a-z]/.test(str)) {  // if não testado
                 console.log("if 2.2");
-                x[count++] = [/^\D+/.exec(str), /\d+/.exec(str) * multi];
+                x[count++] = [/^\D+/.exec(str), /\d+/.exec(str) * num];
                 str = str.slice(1);                                         // Corrigir o slice
-                console.log(str);
             }
             if (/^[A-Z]/.test(str)) {
                 console.log("if 2.3");
-                x[count++] = [/^[A-Z]/.exec(str), /\d+/.exec(str) * multi];
+                x[count++] = [/^[A-Z]/.exec(str), /\d+/.exec(str) * num];
                 str = str.slice(/\D+\d+/.exec(str).toString().length);
-                console.log(str);
-            } else throw "Sequência inválida / não definida";
+            } else throw "Erro no bloco tratarFormula()";
         }
         console.log("<< while >>");
     } while (str);
@@ -89,20 +92,19 @@ function tratarFormula(par, multiplicador) {
 
 // Arquivo de entrada
 const fs = require('fs');
-const arquivo = "./teste1";     // Altere o './testen' para n = '1' até '8'
+//const arquivo = "./teste2";     // Altere o './testen' para n = '1' até '8'
 
 // Manipulação e separação do arquivo de entrada para gerar a saída
 var count = 0;
 var x = [];
-var txt = "C6H12O6";
-var formula = "";
-var formulaAux = "";
+var formula = "Mg(OH)2";
+var elemento = "";
 var multi = 1;
 
 try {
-    //txt = fs.readFileSync(arquivo + ".txt", "UTF-8");
-    console.log("Entrada: " + txt);
-    separarFormula(txt);
+    //formula = fs.readFileSync(arquivo + ".txt", "UTF-8");
+    console.log("Entrada: " + formula);
+    separarFormula();
 
     console.log("-----");
     for (var i = 0; i < count; i++) { console.log(x[i].toString()); }
