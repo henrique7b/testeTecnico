72e7b28fa645151fe288e586e2378d79a418f8de
Henrique Santana <45628939+henrique7b@users.noreply.github.com>
2019-09-13 11:28:28 -0300

    Maior legibilidade

--- decode/app.js
+++ decode/app.js
@@ -1,53 +1,49 @@
-function decodificar(codigo, count) {
-    let strAux = [];
+const fs = require('fs')
+
+'use strict'
+let valueDecoded
+
+const decode = (code) => {
+    let _aux = []
 
     // Criando a primeira coluna
-    if (!valoresDecodificados[0]) {
-        for (let i = 0; i < codigo.length; i++) {
-            strAux[i] = codigo.charAt(i);
+    if (!valueDecoded[0]) {
+        for (let value of code) {
+            _aux.push(value)
         }
-        strAux.sort();
-        for (let i = 0; i < codigo.length; i++) {
-            valoresDecodificados[i] = strAux[i].toString();
-        }
+        _aux.sort()
+        valueDecoded = _aux.map((value) =>
+            value.toString())
     }
 
-    for (let i = count; i > 1; i--) {
-        // Criando as outras colunas
-        for (let i = 0; i < codigo.length; i++) {
-            strAux[i] = codigo.charAt(i);
-            strAux[i] += valoresDecodificados[i].toString();
+    // Criando as outras colunas
+    for (let i = code.length; i > 1; i--) {
+        for (let i = 0; i < code.length; i++) {
+            _aux[i] = code.charAt(i) + valueDecoded[i].toString()
         }
-        for (let i = 0; i < codigo.length; i++) {
-            valoresDecodificados[i] = strAux[i].toString();
-        }
-        for (let i = 0; i < codigo.length; i++) {
-            valoresDecodificados.sort();
-        }
+        valueDecoded = _aux.map((value) =>
+            value.toString())
+        valueDecoded.forEach(() => {
+            valueDecoded.sort()
+        })
     }
 }
 
-// Inicio do código
-const fs = require('fs');
-const nomeArquivo = "./decode-8";   // Altere o './decode-n' para n = '5', '6', '7' ou '8'. './decode-8' foi utilizado apenas como testes
-var valorCodificado;
-var indice;
-var dados;
-var valoresDecodificados;
-
-// Tratamento dos valores obtidos
 try {
-    dados = fs.readFileSync(nomeArquivo + ".in", "UTF-8");
-    valorCodificado = dados.slice(dados.indexOf("['") + 2, dados.indexOf("',"));
-    indice = Number(dados.slice(dados.indexOf(", ") + 2, dados.indexOf("]")));
-    // Decodificação dos valores
-    valoresDecodificados = new Array(valorCodificado.length);
-    decodificar(valorCodificado, valorCodificado.length);
-    // Gerando a saída
-    console.log("Entrada: " + dados + " | Saída: " + valoresDecodificados[indice]);
-    fs.writeFileSync(nomeArquivo + ".out", valoresDecodificados[indice], "UTF-8");
+    for (let i = 5; i <= 8; i++) {
+        let file = `\\decode-${i}`
+        let input = fs.readFileSync(__dirname + file + '.in', 'UTF-8')
+        let valueCoded = input.slice(input.indexOf('[\'') + 2, input.indexOf('\','))
+        let index = Number(input.slice(input.indexOf(', ') + 2, input.indexOf(']')))
 
+        valueDecoded = new Array(valueCoded.length)
+        decode(valueCoded)
+
+        console.log(`Entrada: ${input}\nSaída: ${valueDecoded[index]}\n`)
+        // Descomente para gerar os arquivos de saída
+        // fs.writeFileSync(__dirname + file + '.out', valueDecoded[index], 'UTF-8')
+    }
 } catch (err) {
-    console.log(err.name);
-    console.log(err.message);
+    console.log(err.name)
+    console.log(err.message)
 }
